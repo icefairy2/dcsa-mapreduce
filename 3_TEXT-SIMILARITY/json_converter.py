6e81552f1f3994d0a69b6664cd6d0f011934d4be
Timea Gyarmathy <gyarmathy.timi@hotmail.com>
2021-01-09 13:14:46 +0100

    3TEXTSIM: Added random paper selector

--- 3_TEXT-SIMILARITY/json_converter.py
+++ 3_TEXT-SIMILARITY/json_converter.py
@@ -6,7 +6,11 @@ OUTPUT_FILE = 'arxivData_lines.json'
 with open(INPUT_FILE) as f:
     data = json.load(f)
 
+    f.close()
+
     with open(OUTPUT_FILE, 'w') as json_file:
         for entry in data:
             json.dump(entry, json_file)
             json_file.write('\n')
+
+        json_file.close()
